49474affedb1739179330452ac38abb37eeca0bf
Shubhradeep Bose <75801099+Shubhradeep007@users.noreply.github.com>
2021-05-19 22:18:37 -0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,15 +1,15 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "school";
+$servername = "servername";
+$username = "username";
+$password = "Password";
+$database = "database Name";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn){
     die("Sorry we failed to connect: ". mysqli_connect_error());
 }
 else{
-    // echo "Connection was successful<br>";
+    // echo "Connection was successful<br>"; // Enable this to show your connection was establsih or not
 }
 
 
